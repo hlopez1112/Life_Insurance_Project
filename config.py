544dc5bfa764308c0b62a999997f4cb7ba7ae829
Hector Lopez <lopezh1112@gmail.com>
2026-09-22 19:55:00 -0700

    Remove files

--- config.py
+++
@@ -1,8 +0,0 @@
-from models.assumptions import Assumptions
-
-DEFAULT_ASSUMPTIONS = Assumptions(
-    interest_rate=0.05,
-    expense_ratio=0.10,
-    policy_fee=50,
-    acquisition_cost=500,
-    )
