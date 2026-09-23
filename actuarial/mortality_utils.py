8ddb0d4b3a948c3a6140459ef3226c9eafc4f420
Hector Lopez <lopezh1112@gmail.com>
2026-09-22 18:24:58 -0700

    Created a utils file for mortality functions and added comments

---
+++ actuarial/mortality_utils.py
@@ -0,0 +1,44 @@
+import numpy as np
+import pandas as pd
+
+
+
+def get_qx(df: pd.DataFrame, age: int, risk_class: str, gender: str) -> np.ndarray:
+
+    '''
+    Lookup the mortality rates for a given age, risk class, and gender from the provided DataFrame.
+    Returns a numpy array of mortality rates for the specified age, risk class, and gender.
+    '''
+
+    # Create a boolean mask to filter the DataFrame based on the provided age, risk class, and gender
+    mask = (
+    (df["issue_age"] == age)
+    & (df["risk_class"] == risk_class)
+    & (df["gender"] == gender)
+    )
+
+    result = df.loc[mask]
+
+    if result.empty:
+        raise ValueError(f"No mortality data for age {age}")
+
+    return result.drop(columns=["issue_age", "risk_class", "gender"]).values[0]
+
+
+def death_probabilities(mortality_values: np.ndarray, term: int)-> tuple[np.ndarray, np.ndarray]:
+    '''
+    Calculate the survival and death probabilities for a given term based on mortality values.  
+    Returns a tuple containing two numpy arrays: survival probabilities and death probabilities.
+    '''
+
+    # initialize survival probabilities and death probabilities arrays
+    mortality_term = mortality_values[:term]
+    survival_probabilities = np.empty_like(mortality_term)
+
+    survival_probabilities[0] = 1
+
+    # Calculate the survival probabilities and death probabilities for each year in the term
+    survival_probabilities[1:] = np.cumprod(1 - mortality_term)[:-1]
+    death_probabilities = survival_probabilities * mortality_term
+
+    return survival_probabilities, death_probabilities
